8ec49f3befd90cffef96b97992558f48c482d4f6
Parker <parkerkrahn10@gmail.com>
2025-07-01 17:43:59 -0700

    Complete code

--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,10 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  for (let i = 1; i <= 3; i++) {
+    const folder = await createFolder("Folder " + i);
+    for (let j = 1; j <= 5; j++) {
+      await createFile("File " + j, 1000 * j, folder.id);
+    }
+  }
 }
